3cf98e0db808eebcb03fc67063856bd58d3af278
tanni6808 <tanni6808450@gmail.com>
2025-03-11 15:41:31 +0800

    Modified app.py, add get /api/attractions

--- app.py
+++ app.py
@@ -1,5 +1,22 @@
 from fastapi import *
 from fastapi.responses import FileResponse
+from typing import Annotated
+import mysql.connector
+import mysql.connector.pooling
+
+dbconfig = {
+	'user': 'root',
+	'password': '123456',
+	'host': 'localhost',
+	'database': 'taipei_day_trip'
+}
+
+mydbpool=mysql.connector.pooling.MySQLConnectionPool(
+	pool_name='mypool', 
+	pool_size=10,
+	**dbconfig
+)
+
 app=FastAPI()
 
 # Static Pages (Never Modify Code in this Block)
@@ -15,3 +32,29 @@ async def booking(request: Request):
 @app.get("/thankyou", include_in_schema=False)
 async def thankyou(request: Request):
 	return FileResponse("./static/thankyou.html", media_type="text/html")
+
+@app.get("/api/attractions")
+async def get_attraction(page: int, keyword: str | None=None):
+	try:
+		mydb=mydbpool.get_connection()
+		mycursor=mydb.cursor(dictionary=True)
+		if keyword==None:
+			mycursor.execute('SELECT id, name, category, description, address, transport, mrt, ST_X(coordinate) AS lat, ST_Y(coordinate) AS lng, images FROM attractions;')
+			myresult=mycursor.fetchall()
+			mycursor.close()
+			mydb.close()
+		else:
+			mycursor.execute("SELECT id, name, category, description, address, transport, mrt, ST_X(coordinate) AS lat, ST_Y(coordinate) AS lng, images FROM attractions WHERE name LIKE %s or mrt LIKE %s;", ('%'+ keyword + '%', '%'+ keyword + '%'))
+			myresult=mycursor.fetchall()
+			mycursor.close()
+			mydb.close()
+		for result in myresult:
+			result['images']=result['images'].split(' ')
+		data=myresult[page*12:page*12+12]
+		if len(myresult)<page*12+13:
+			next_page=None
+		else:
+			next_page=page+1
+		return {"nextPage": next_page, "data": data}
+	except:
+		return {"error": True, "message": "無法取得資料"}
